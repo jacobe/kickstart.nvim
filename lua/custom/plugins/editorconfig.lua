f9c0a65d099658e0a434bf4b2bbc374e35fddccb
Jacob Eisenberg <jacob@creave.dk>
2025-11-11 12:37:44 +0100

    Add editorconfig plugin

---
+++ lua/custom/plugins/editorconfig.lua
@@ -0,0 +1,6 @@
+-- EditorConfig plugin for Neovim
+-- Automatically applies editor settings from .editorconfig files
+-- See: https://editorconfig.org/
+return {
+  'editorconfig/editorconfig-vim',
+}
