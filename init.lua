25c8c8d34b6977e1ce4cd86e0b6ae331ad73170f
Jacob Eisenberg <jacob@creave.dk>
2025-11-17 10:32:03 +0100

    Install Python and PHP linters

--- init.lua
+++ init.lua
@@ -689,6 +689,7 @@ require('lazy').setup({
       local servers = {
         -- clangd = {},
         -- gopls = {},
+        ruff = {},
         pyright = {
           settings = {
             python = {
@@ -700,16 +701,6 @@ require('lazy').setup({
             },
           },
         },
-        -- rust_analyzer = {},
-        -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
-        --
-        -- Some languages (like typescript) have entire language plugins that can be useful:
-        --    https://github.com/pmizio/typescript-tools.nvim
-        --
-        -- But for many setups, the LSP (`ts_ls`) will work just fine
-        -- ts_ls = {},
-        --
-
         lua_ls = {
           -- cmd = { ... },
           -- filetypes = { ... },
@@ -724,6 +715,8 @@ require('lazy').setup({
             },
           },
         },
+        intelephense = {},
+        laravel_ls = {},
       }
 
       -- Ensure the servers and tools above are installed
@@ -742,7 +735,6 @@ require('lazy').setup({
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
         'stylua', -- Used to format Lua code
-        'ruff', -- Python formatter and linter (fast, modern)
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
--- lua/custom/plugins/conform.lua
+++ lua/custom/plugins/conform.lua
@@ -22,6 +22,7 @@ return {
     end,
     formatters_by_ft = {
       lua = { 'stylua' },
+      php = { 'pint' },
       -- Conform can also run multiple formatters sequentially
       python = { 'ruff_format', 'ruff_organize_imports' },
       -- You can use 'stop_after_first' to run the first available formatter from the list
